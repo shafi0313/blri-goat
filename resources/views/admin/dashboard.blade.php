e7cebfb5920431708ae9b658958d1f57e4453ddb
Shafi <msh.shafiul@gmail.com>
2021-08-23 17:59:29 +0600

    change: dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -23,8 +23,8 @@
 <div class="main-panel">
     <div class="content">
         <div class="page-inner">
-            <div class="page-header">
-                <h4 class="page-title">Dashboard</h4>
+            <div class="page-header mb-4">
+                <h4 class="page-title" style="font-size: 25px">BLRI Research Farm</h4>
                 {{-- <div class="btn-group btn-group-page-header ml-auto">
                     <button type="button" class="btn btn-light btn-round btn-page-header-dropdown dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-ellipsis-h"></i>
@@ -39,6 +39,106 @@
                     </div>
                 </div> --}}
             </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                          <a class="nav-link active" id="pills-goat-tab" data-toggle="pill" href="#pills-goat" role="tab" aria-controls="pills-goat" aria-selected="true">Goat</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" id="pills-sheep-tab" data-toggle="pill" href="#pills-sheep" role="tab" aria-controls="pills-sheep" aria-selected="false">Sheep</a>
+                        </li>
+                      </ul>
+                      {{-- <div class="text-center mb-2" style="font-size: 25px;">Dashboard (Goat)</div> --}}
+                      <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-goat" role="tabpanel" aria-labelledby="pills-goat-tab">
+
+                            <div class="row">
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="card card-stats card-primary card-round">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-5">
+                                                    <div class="icon-big text-center">
+                                                        <i class="flaticon-users"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col col-stats">
+                                                    <div class="numbers">
+                                                        <p class="card-category">Visitors</p>
+                                                        <h4 class="card-title">1,294</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="card card-stats card-info card-round">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-5">
+                                                    <div class="icon-big text-center">
+                                                        <i class="flaticon-interface-6"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col col-stats">
+                                                    <div class="numbers">
+                                                        <p class="card-category">Subscribers</p>
+                                                        <h4 class="card-title">1303</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="card card-stats card-success card-round">
+                                        <div class="card-body ">
+                                            <div class="row">
+                                                <div class="col-5">
+                                                    <div class="icon-big text-center">
+                                                        <i class="flaticon-analytics"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col col-stats">
+                                                    <div class="numbers">
+                                                        <p class="card-category">Sales</p>
+                                                        <h4 class="card-title">$ 1,345</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="card card-stats card-secondary card-round">
+                                        <div class="card-body ">
+                                            <div class="row">
+                                                <div class="col-5">
+                                                    <div class="icon-big text-center">
+                                                        <i class="flaticon-success"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col col-stats">
+                                                    <div class="numbers">
+                                                        <p class="card-category">Order</p>
+                                                        <h4 class="card-title">576</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="pills-sheep" role="tabpanel" aria-labelledby="pills-sheep-tab">...</div>
+                      </div>
+                </div>
+            </div>
+
+
             <style>
                 .card-category {
                     font-size: 14px;
@@ -50,7 +150,7 @@
                 }
             </style>
 
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col">
                     <div id="total_animal" ></div>
                 </div>
@@ -60,7 +160,7 @@
                 <div class="col">
                     <div id="total_sheep" ></div>
                 </div>
-            </div>
+            </div> --}}
 
 
         </div>
